83b05e4e2581222295440a136d3781cc3ff08cfb
Patrick Oliveira <joaopatrick65@gmail.com>
2024-09-17 15:33:35 -0300

    aulas 13 e 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+let cont = 1
+
+do {
+    console.log(`Passo ${cont}`)
+    cont++
+} while(cont <= 6)
+
+
+// let cont = 1
+
+// while(cont <= 6) {
+//     console.log(`Passo ${cont}`)
+//     cont++
+// }
---
+++ aula14/ambiente.js
@@ -0,0 +1,9 @@
+let i = 1
+while (i <= 5) {
+    console.log(i)
+    i++
+}
+
+for (let j = 10; j > 0; j--) {
+    console.log(j)
+}
